f92cfe590368dcad5101590dadc4bec64051c09c
David Vilaça <vilacapdavid@gmail.com>
2019-09-23 09:46:30 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'barbex-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'barbex-front-manager';
+}
