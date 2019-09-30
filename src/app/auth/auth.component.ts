2980f3c70fd167c74b0071a9936b2bb39095ec30
David Vilaça <vilacapdavid@gmail.com>
2019-09-30 20:58:27 -0300

    refactor: improve auth compoent style

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '@environments';
 
 @Component({
   selector: 'barbex-auth',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+
+  version = environment.version;
 
   constructor() { }
 
