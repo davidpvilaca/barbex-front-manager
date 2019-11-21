a2eb7537ecd4f0f52ae64b80a727eb4d4869bfc3
David Vilaça <vilacapdavid@gmail.com>
2019-11-21 10:51:06 -0300

    feat: add navbar module

---
+++ src/app/@shared/navbar/navbar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '@core/providers';
+
+@Component({
+  selector: 'barbex-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  show = false;
+  user: any;
+
+  constructor(private readonly auth: AuthService, private readonly router: Router) { }
+
+  ngOnInit() {
+    this.user = this.auth.currentUser;
+    document.querySelectorAll('ul.navbar-nav li.nav-item a.nav-link').forEach((element: Element & { onclick: () => any }) => {
+      element.onclick = () => this.show = false;
+    });
+  }
+
+  logout(): void {
+    this.auth.logout();
+    this.router.navigate(['/auth/login']);
+  }
+
+}
