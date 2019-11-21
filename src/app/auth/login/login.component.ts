72c0c605247c825257e7f01a2328ba5477603b47
David Vilaça <vilacapdavid@gmail.com>
2019-11-21 10:46:14 -0300

    feat: auth module calling api authentication

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { finalize } from 'rxjs/operators';
+
+import { AuthService, LoadingService } from '@core/providers';
 
 @Component({
   selector: 'barbex-login',
@@ -7,9 +13,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  private redirectTo: string;
+  constructor(private readonly formBuilder: FormBuilder, private readonly route: ActivatedRoute,
+    private readonly messageService: MessageService, private readonly auth: AuthService,
+    private readonly loading: LoadingService, private readonly router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+
+    const signupSuccess = this.route.snapshot.queryParamMap.get('signupSuccess');
+    if (signupSuccess === 'true') {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Tudo certo',
+        detail: 'Conta cadastrada com sucesso!'
+      });
+    }
+
+    this.redirectTo = this.route.snapshot.queryParamMap.get('redirectTo');
+  }
+
+  getFormFieldError(formControlName: string): string | null {
+    const formControl = this.form.get(formControlName);
+    if (formControl && formControl.invalid) {
+      if (formControl.hasError('required')) {
+        return 'Campo obrigatório';
+      }
+      if (formControl.hasError('email')) {
+        return 'Email inválido';
+      }
+      if (formControl.hasError('minlength')) {
+        return 'Mínimo 6 caracteres';
+      }
+    }
+    return null;
+  }
+
+  submit() {
+    this.toutchForm();
+    if (this.form.invalid) { return; }
+    this.loading.setLoading();
+    const { email, password } = this.form.value;
+    this.auth.login(email, password).pipe(
+      finalize(() => this.loading.clearLoading())
+    ).subscribe(
+      () => {
+        this.router.navigate(this.redirectTo ? this.redirectTo.split('/').map(a => !a ? '/' : a) : ['/app']);
+      },
+      err => {
+        console.log(err);
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Não autorizado',
+          detail: 'Usuário ou senha inválido!'
+        });
+      }
+    );
+  }
+
+  private toutchForm(): void {
+    if (this.form.touched) { return; }
+    Object.keys(this.form.controls).forEach(key => this.form.controls[key].markAsTouched());
   }
 
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+
+import { LoadingService, AuthService } from '@core/providers';
 
 @Component({
   selector: 'barbex-signup',
@@ -7,9 +12,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  constructor(private readonly formBuilder: FormBuilder, private readonly loading: LoadingService,
+    private readonly auth: AuthService, private readonly router: Router) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), this.checkPasswords.bind(this)]],
+      repassword: ['', [Validators.required, this.checkPasswords.bind(this)]]
+    });
+
+    const password = this.form.get('password');
+    const repassword = this.form.get('repassword');
+    repassword.valueChanges.subscribe(() => {
+      if (password && password.errors && password.errors.repassword) {
+        if (Object.keys(password.errors).length === 1) {
+          password.reset(password.value);
+        }
+      }
+    });
+  }
+
+  submit() {
+    this.toutchForm();
+    if (this.form.invalid) { return; }
+    this.loading.setLoading();
+    this.auth.signup(this.form.value).pipe(
+      finalize(() => this.loading.clearLoading())
+    ).subscribe(
+      () => {
+        this.router.navigate(['/auth/login'], { queryParams: { signupSuccess: true } });
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  getFormFieldError(formControlName: string): string | null {
+    const formControl = this.form.get(formControlName);
+    if (formControl && formControl.invalid) {
+      if (formControl.hasError('required')) {
+        return 'Campo obrigatório';
+      }
+      if (formControl.hasError('email')) {
+        return 'Email inválido';
+      }
+      if (formControl.hasError('minlength')) {
+        return 'Mínimo 6 caracteres';
+      }
+      if (formControl.hasError('repassword')) {
+        return 'Senhas não correspondem';
+      }
+    }
+    return null;
+  }
+
+  private toutchForm(): void {
+    if (this.form.touched) { return; }
+    Object.keys(this.form.controls).forEach(key => this.form.controls[key].markAsTouched());
+  }
+
+  private checkPasswords() {
+    if (!this.form) { return null; }
+    const pass = this.form.get('password').value;
+    const confirmPass = this.form.get('repassword').value;
+    return pass === confirmPass ? null : { repassword: true };
   }
 
 }
